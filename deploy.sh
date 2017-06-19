e525af8fa84419d2029060a3517c86ad4957f185
Jacque Schrag <jnschrag@gmail.com>
2017-06-19 15:25:13 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 chmod 600 /tmp/mt_rsa
 eval "$(ssh-agent -s)" # Start the ssh agent
-ssh-add /tmp/mt_travis
+ssh-add /tmp/mt_rsa
 git remote add missilethreat_timeline git@git.wpengine.com:production/missile.git # add remote for production
-git fetch --unshallow # fetch all repo history or wpengine complains
-git push -f missilethreat_timeline master #deploy to staging site from master branch
+git push missilethreat_timeline master #deploy to staging site from master branch
