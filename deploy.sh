37d9bb05a3225fb7593e38017d3d3d1f8e9399a2
Serven Maraghi <maraghiserven@gmail.com>
2019-12-09 15:46:21 -0500

    Remove remaining travis ci files

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-chmod 600 /tmp/mt_rsa
-eval "$(ssh-agent -s)" # Start the ssh agent
-ssh-add /tmp/mt_rsa
-git remote add missilethreat_timeline git@git.wpengine.com:production/missile.git # add remote for production
-git push missilethreat_timeline master #deploy to staging site from master branch
