0825a054d7ed35df2e09a7ddf3e11303dc57bde7
Lindsay Urchyk <urchykli@gmail.com>
2020-03-03 13:51:25 -0500

    Download link target blank

--- MissileThreat-timeline/js/bar.js
+++ MissileThreat-timeline/js/bar.js
@@ -200,7 +200,7 @@ function renderChart(series, drilldown) {
     },
     subtitle: {
       text:
-        'Interactive Timeline<br/>Click on a bar to see individual events in a month<br/><a href="https://docs.google.com/spreadsheets/d/1ZY_g-shtct4IJZQFiFdIp-DZ7WXAL4IssWmMaKZRsb4/edit?usp=sharing" className="dataLink">Download data in spreadsheet</a>',
+        'Interactive Timeline<br/>Click on a bar to see individual events in a month<br/><a href="https://docs.google.com/spreadsheets/d/1ZY_g-shtct4IJZQFiFdIp-DZ7WXAL4IssWmMaKZRsb4/edit?usp=sharing" target="_blank" className="dataLink">Download data in spreadsheet</a>',
       useHTML: true,
     },
     // Credits
